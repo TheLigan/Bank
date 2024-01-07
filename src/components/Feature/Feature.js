5974f9c551cfb703c02862229501707eca9bd6a5
Verdon Xavier <verdonxavier2@gmail.com>
2024-01-07 22:29:02 +0100

    Premier ajout.

---
+++ src/components/Feature/Feature.js
@@ -0,0 +1,15 @@
+
+
+function Feature({imgSrc, imgAlt, titre, description}) {
+    return (
+        <div className="feature-item">
+            <img src={imgSrc} alt={imgAlt} className="feature-icon" />
+            <h3 className="feature-item-title">{titre}</h3>
+            <p>
+                {description}
+            </p>
+        </div>
+    );
+}
+
+export default Feature;
---
+++ src/pages/404/404.js
@@ -0,0 +1,17 @@
+import { NavLink } from "react-router-dom";
+
+
+function Erreur404() {
+    return(
+        <main className="bg-dark">
+            <section className="erreur-404">
+                <h1>Error 404</h1>
+                <NavLink to="/" className="link-btn-404">
+                    <button className="btn-404">Back to home</button>
+                </NavLink>
+            </section>
+        </main>
+    );
+}
+
+export default Erreur404;
